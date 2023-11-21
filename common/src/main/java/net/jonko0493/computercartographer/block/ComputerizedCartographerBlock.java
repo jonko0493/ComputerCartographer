47dfffd646e8bf231c83f85753ded080d7bd59ea
jonko0493 <jonko0493@protonmail.com>
2023-11-20 23:08:44 -0800

    Add assets

--- common/src/main/java/net/jonko0493/computercartographer/block/ComputerizedCartographerBlock.java
+++ common/src/main/java/net/jonko0493/computercartographer/block/ComputerizedCartographerBlock.java
@@ -1,11 +1,14 @@
 package net.jonko0493.computercartographer.block;
 
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nonnull;
 
 public class ComputerizedCartographerBlock extends BlockWithEntity {
     public ComputerizedCartographerBlock(AbstractBlock.Settings settings) {
@@ -18,13 +21,9 @@ public class ComputerizedCartographerBlock extends BlockWithEntity {
         return new ComputerizedCartographerBlockEntity(pos, state);
     }
 
-//    @Nullable
-//    @Override
-//    public BlockState getPlacementState(ItemPlacementContext ctx) {
-//        Direction direction = ctx.getHorizontalPlayerFacing().getOpposite();
-//        if (direction == Direction.DOWN || direction == Direction.UP) {
-//            direction = Direction.NORTH;
-//        }
-//        return this.getDefaultState().with(FacingBlock.FACING, direction);
-//    }
+    @Nonnull
+    @Override
+    public BlockRenderType getRenderType(@Nonnull BlockState state) {
+        return BlockRenderType.MODEL;
+    }
 }
---
+++ common/src/main/resources/assets/computer_cartographer/textures/block/reuse.md
@@ -0,0 +1,2 @@
+# Reuse Statement
+The `computer_normal_side` and `computer_normal_top` assets are taken from the [CC-Tweaked](https://github.com/cc-tweaked/CC-Tweaked) project. They are licensed under a Creative Commons attribution license.
